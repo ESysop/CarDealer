649a50ecfd8daf32a5078ba231d8bb5b49226632
esysop <leehassel@gmail.com>
2016-04-14 16:46:27 -0500

    Working display od Date

---
+++ OverLoadOpeator/OverLoadOpeator/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverLoadOpeator
+	{
+	class Person
+		{
+		public string firstName, lastName,birthDay;
+		public int currentAge, adjustedAge, amountToAdjust, year, month, day;
+		DateTime dateTime;
+
+
+		public Person ()
+			{
+			dateTime = new DateTime();
+		
+			}
+		public void askDate ()
+			{
+			Console.WriteLine("Enter your birthdate" );
+			birthDay = Console.ReadLine();
+			dateTime = Convert.ToDateTime(birthDay);
+			Console.WriteLine(dateTime.ToShortDateString());
+			Console.ReadLine();
+			}
+		
+
+		}
+	}
+	
--- OverLoadOpeator/OverLoadOpeator/Program.cs
+++ OverLoadOpeator/OverLoadOpeator/Program.cs
@@ -8,8 +8,13 @@ namespace OverLoadOpeator
 	{
 	class Program
 		{
+		
 		static void Main (string[] args)
 			{
+			Person person = new Person();
+			person.askDate();
+
 			}
+		
 		}
 	}
